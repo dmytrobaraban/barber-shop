c1ac36eab49bce6e77cfce83f91b9ee401a3d4a3
DmytroB <dmitrijbaraban@gmail.com>
2023-02-25 19:40:24 +0200

    complite services

--- js/index.js
+++ js/index.js
@@ -81,18 +81,19 @@ const closeModalWindow = () => {
 closeModalWindow();
 
 const submitBtn = document.getElementById('submit');
+
 submitBtn.addEventListener('click', () => {
-  const pElem = document.querySelector('.success');
-  const dateField = document.getElementById('date');
-  const mounth = dateField.value.slice(5, 7);
-  const day = dateField.value.slice(8, 10);
-  const time = dateField.value.slice(11, 16);
-  const message = `Ви успішно записалися на ${day}.${mounth}<span>До зустрічі об ${time}</span><button class="close-message">Добре</button>`;
-  pElem.innerHTML = message;
-  const pElemBtn = document.querySelector('.close-message');
-  pElemBtn.addEventListener('click', () => {
-    pElem.innerHTML = '';
-  });
+const dateField = document.getElementById('date');
+const pElem = document.querySelector('.success');
+ const mounth = dateField.value.slice(5, 7);
+ const day = dateField.value.slice(8, 10);
+ const time = dateField.value.slice(11, 16);
+ const message = `Ви успішно записалися на ${day}.${mounth}<span>До зустрічі об ${time}</span><button class="close-message">Добре</button>`;
+ pElem.innerHTML = message;
+ const pElemBtn = document.querySelector('.close-message');
+ pElemBtn.addEventListener('click', () => {
+   pElem.innerHTML = '';
+ }); 
 });
 
 // функція для валідації (перевірки) на введення даних користувачем
