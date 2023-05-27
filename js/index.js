d51d45574a084553fa9f5c6488d3069e0dfebb50
DmytroB <dmitrijbaraban@gmail.com>
2023-05-27 20:05:42 +0300

    add save and display ls

--- js/index.js
+++ js/index.js
@@ -110,58 +110,111 @@ const dateField = document.getElementById('date');
 const currentDate = new Date().toISOString().slice(0, 16);
 dateField.setAttribute('min', currentDate);
 
-function validate(event) {
-  event.preventDefault();
-  let valid = false;
+/* Після кожного оновлення сторінки спрацьовує наступна функція */
 
+document.addEventListener('DOMContentLoaded', () => {
   const nameField = document.getElementById('name');
   const phoneField = document.getElementById('phone');
   const dateField = document.getElementById('date');
+  const tableOfRecord = document.getElementById('records');
 
-  if (!nameField.value) {
-    document.querySelector('.input-name').className =
-      'input-name validation-error';
-    valid = false;
-  } else {
-    valid = true;
+  // Перевіряємо, чи є дані у localStorage
+  if (localStorage.getItem('formData')) {
+    const formData = JSON.parse(localStorage.getItem('formData'));
+    nameField.value = formData.name || '';
+    phoneField.value = formData.phone || '';
+    dateField.value = formData.date || '';
   }
 
-  if (!phoneField.value) {
-    document.querySelector('.input-tel').className =
-      'input-tel validation-error';
-    valid = false;
-  } else {
-    valid = true;
+  function validate(event) {
+    event.preventDefault();
+    let valid = false;
+
+    const nameField = document.getElementById('name');
+    const phoneField = document.getElementById('phone');
+    const dateField = document.getElementById('date');
+
+    if (!nameField.value) {
+      document.querySelector('.input-name').className =
+        'input-name validation-error';
+      valid = false;
+    } else {
+      valid = true;
+    }
+
+    if (!phoneField.value) {
+      document.querySelector('.input-tel').className =
+        'input-tel validation-error';
+      valid = false;
+    } else {
+      valid = true;
+    }
+
+    if (!dateField.value) {
+      document.querySelector('.input-date').className =
+        'input-date validation-error';
+      valid = false;
+    } else {
+      valid = true;
+    }
+
+    if (valid) {
+      // Розбиваємо на окремі функції для більшої зручності
+      saveFormData();
+      displayRecord();
+
+      const pElem = document.querySelector('.success');
+      const month = dateField.value.slice(5, 7);
+      const day = dateField.value.slice(8, 10);
+      const time = dateField.value.slice(11, 16);
+      pElem.classList.add('active');
+      const message = `Ви успішно записалися на ${day}.${month}<br>До зустрічі об ${time}<button class="close-message">Добре</button>`;
+      pElem.innerHTML = message;
+      const pElemBtn = document.querySelector('.close-message');
+      pElemBtn.addEventListener('click', () => {
+        pElem.innerHTML = '';
+      });
+
+      nameField.value = ''; // Очищення поля для імені
+      phoneField.value = ''; // Очищення поля для номера телефону
+      dateField.value = ''; // Очищення поля для дати та часу
+    }
   }
 
-  if (!dateField.value) {
-    document.querySelector('.input-date').className =
-      'input-date validation-error';
-    valid = false;
-  } else {
-    valid = true;
+  function saveFormData() {
+    const formData = {
+      name: nameField.value,
+      phone: phoneField.value,
+      date: dateField.value,
+    };
+    let savedData = [];
+    if (localStorage.getItem('savedData')) {
+      savedData = JSON.parse(localStorage.getItem('savedData'));
+    }
+    savedData.push(formData);
+    localStorage.setItem('savedData', JSON.stringify(savedData));
   }
 
-  if (valid) {
-    const pElem = document.querySelector('.success');
-    const tableOfRecord = document.getElementById('records');
-    const month = dateField.value.slice(5, 7);
-    const day = dateField.value.slice(8, 10);
-    const time = dateField.value.slice(11, 16);
-    pElem.classList.add('active');
-    const message = `Ви успішно записалися на ${day}.${month}<br>До зустрічі об ${time}<button class="close-message">Добре</button>`;
-    pElem.innerHTML = message;
-    const record = `<tr><td>${nameField.value}</td><td>${day}.${month}</td><td>${time}</td></tr>`;
-    tableOfRecord.innerHTML += record;
-    const pElemBtn = document.querySelector('.close-message');
-    pElemBtn.addEventListener('click', () => {
-      pElem.innerHTML = '';
-    });
-    nameField.value = ''; // Очищення поля для імені
-    phoneField.value = ''; // Очищення поля для номера телефону
-    dateField.value = ''; // Очищення поля для дати та часу
+  function displayRecord() {
+    const savedData = JSON.parse(localStorage.getItem('savedData'));
+    tableOfRecord.innerHTML = '';
+    if (savedData && savedData.length > 0) {
+      savedData.forEach((data) => {
+        const { name, phone, date } = data;
+        const month = date.slice(5, 7);
+        const day = date.slice(8, 10);
+        const time = date.slice(11, 16);
+        const record = `<tr><td>${name}</td><td>${day}.${month}</td><td>${time}</td></tr>`;
+        tableOfRecord.innerHTML += record;
+      });
+    }
   }
-}
+
+  const form = document.querySelector('.modal-form');
+  form.addEventListener('submit', validate);
+
+  displayRecord(); // Виведення списку при завантаженні сторінки
+});
 
 let docTitle = document.title;
 window.addEventListener('blur', () => {
